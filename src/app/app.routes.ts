44cbd557b3c7a3f6cc90db2dc79345537459707a
Jeremy Black <blackjf85@gmail.com>
2025-01-13 22:12:02 +0700

    feat: setup basic routing structure with home, about and not-found components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+    title: 'Home',
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./about/about.component').then((m) => m.AboutComponent),
+    title: 'About',
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./not-found/not-found.component').then(
+        (m) => m.NotFoundComponent
+      ),
+    title: 'Page Not Found',
+  },
+];
