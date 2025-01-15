cb43fd3230f0648da52f6a6d5e14fdbe8ebbd19a
Jeremy Black <blackjf85@gmail.com>
2025-01-16 06:27:08 +0700

    Implemented modal component with basic, full-screen, and header/footer variants

--- libs/shared/ui-components/src/lib/ui-components.ts
+++ libs/shared/ui-components/src/lib/ui-components.ts
@@ -1,3 +1,4 @@
 export { ButtonComponent } from './button/button.component';
 export { CardComponent } from './card/card.component';
 export { InputComponent } from './input/input.component';
+export { ModalComponent } from './modal/modal.component';
